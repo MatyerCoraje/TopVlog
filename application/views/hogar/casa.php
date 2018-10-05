9f2c3cca055093a17ab3f0e043f653ff76bf25f6
MatyerCoraje <43730705+MatyerCoraje@users.noreply.github.com>
2018-10-05 03:21:11 -0500

    20181005_0321
    20181005_0321

--- application/views/hogar/casa.php
+++ application/views/hogar/casa.php
@@ -21,6 +21,94 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<body>
 			<input id="search" autocapitalize="none" autocomplete="off" autocorrect="off" name="search_query" tabindex="0" spellcheck="false" placeholder="Buscar" aria-label="Buscar" aria-haspopup="false" role="combobox" aria-autocomplete="list" dir="ltr" style="outline: currentcolor none medium;" class="ytd-searchbox" type="text">
 			<div class="cuerpo">
+				<div class="seccionPrincipal"> 
+					<div class="persona">
+						<img src="<?php echo base_url(); ?>recursos/img/vloggerProfile/3.jpg" alt="Nombre" title="descripcion">
+					</div>
+					<div class="videoPrincipal">
+						<embed src="https://www.youtube.com/v/tgbNymZ7vqY">
+					</div>
+				</div>
+				<div class="seccionPrincipal">
+					<div class="persona">
+						<img src="<?php echo base_url(); ?>recursos/img/vloggerProfile/mujer.jpg" alt="Nombre" title="descripcion">
+					</div>
+					<div class="videoPrincipal">
+						<embed src="https://youtu.be/abdN4bSd8aE">
+					</div>
+				</div>
+				<div class="seccionPrincipal">
+					<div class="persona">
+						<img src="<?php echo base_url(); ?>recursos/img/vloggerProfile/3.jpg" alt="Nombre" title="descripcion">
+					</div>
+					<div class="videoPrincipal">
+						<embed src="https://www.youtube.com/v/tgbNymZ7vqY">
+					</div>
+				</div>
+				<div class="seccionPrincipal">
+					<div class="persona">
+						<img src="<?php echo base_url(); ?>recursos/img/vloggerProfile/3.jpg" alt="Nombre" title="descripcion">
+					</div>
+					<div class="videoPrincipal">
+						<embed src="https://www.youtube.com/v/tgbNymZ7vqY">
+					</div>
+				</div>
+				<div class="seccionPrincipal">
+					<div class="persona">
+						<img src="<?php echo base_url(); ?>recursos/img/vloggerProfile/3.jpg" alt="Nombre" title="descripcion">
+					</div>
+					<div class="videoPrincipal">
+						<embed src="https://www.youtube.com/v/tgbNymZ7vqY">
+					</div>
+				</div>
+				<div class="seccionPrincipal">
+					<div class="persona">
+						<img src="<?php echo base_url(); ?>recursos/img/vloggerProfile/3.jpg" alt="Nombre" title="descripcion">
+					</div>
+					<div class="videoPrincipal">
+						<embed src="https://www.youtube.com/v/tgbNymZ7vqY">
+					</div>
+				</div>
+				<div class="seccionPrincipal">
+					<div class="persona">
+						<img src="<?php echo base_url(); ?>recursos/img/vloggerProfile/3.jpg" alt="Nombre" title="descripcion">
+					</div>
+					<div class="videoPrincipal">
+						<embed src="https://www.youtube.com/v/tgbNymZ7vqY">
+					</div>
+				</div>
+				<div class="seccionPrincipal">
+					<div class="persona">
+						<img src="<?php echo base_url(); ?>recursos/img/vloggerProfile/3.jpg" alt="Nombre" title="descripcion">
+					</div>
+					<div class="videoPrincipal">
+						<embed src="https://www.youtube.com/v/tgbNymZ7vqY">
+					</div>
+				</div>
+				<div class="seccionPrincipal">
+					<div class="persona">
+						<img src="<?php echo base_url(); ?>recursos/img/vloggerProfile/3.jpg" alt="Nombre" title="descripcion">
+					</div>
+					<div class="videoPrincipal">
+						<embed src="https://www.youtube.com/v/tgbNymZ7vqY">
+					</div>
+				</div>
+				<div class="seccionPrincipal">
+					<div class="persona">
+						<img src="<?php echo base_url(); ?>recursos/img/vloggerProfile/3.jpg" alt="Nombre" title="descripcion">
+					</div>
+					<div class="videoPrincipal">
+						<embed src="https://www.youtube.com/v/tgbNymZ7vqY">
+					</div>
+				</div>
+				<div class="seccionPrincipal">
+					<div class="persona">
+						<img src="<?php echo base_url(); ?>recursos/img/vloggerProfile/3.jpg" alt="Nombre" title="descripcion">
+					</div>
+					<div class="videoPrincipal">
+						<embed src="https://www.youtube.com/v/tgbNymZ7vqY">
+					</div>
+				</div>
 				<div class="seccionPrincipal">
 					<div class="persona">
 						<img src="<?php echo base_url(); ?>recursos/img/vloggerProfile/3.jpg" alt="Nombre" title="descripcion">
